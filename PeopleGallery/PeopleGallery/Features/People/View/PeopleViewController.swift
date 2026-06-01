c4db361e6f1104f9604a214d8f1b22a4a67b3291
karlaemartins <karlaemartinss@gmail.com>
2026-06-01 11:16:44 -0300

    Setup programmatic UIKit architecture

---
+++ PeopleGallery/PeopleGallery/Features/People/View/PeopleViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PeopleViewController.swift
+//  PeopleGallery
+//
+//  Created by Karla E. Martins Fernandes on 01/06/26.
+//
+
+import UIKit
+
+final class PeopleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "People Gallery"
+        view.backgroundColor = .systemBackground
+        
+    }
+}
